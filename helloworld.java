ff49fef1b28ca3d3206aff6300417d942e26f085
Ankit Panjwani <ankitpanjwani05@gmail.com>
2020-05-13 15:54:51 +0530

    added a comment line for new print statment

--- helloworld.java
+++ helloworld.java
@@ -1,10 +1,11 @@
 
 // this will print hello world in screen
-:
+
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Welcome to GIT !!");
+	//This line will print "Welcome to GIT!!" in the screen
 
 
     }
